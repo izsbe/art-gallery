5278cf4ae7a9ef3325359480fae4c66358a95adf
izsbe <isabella.petsalo@me.com>
2026-03-29 21:13:10 +0300

    Add registration/login/logout

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import sqlite3
 from flask import Flask
+from flask import redirect, render_template, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
+import config
+import db
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return "tähän tulee art gallery"
+    return render_template("index.html")
 
 @app.route("/register")
 def register():
@@ -16,13 +22,36 @@ def create():
     password1 = request.form["password1"]
     password2 = request.form["password2"]
     if password1 != password2:
-        return "VIRHE: salasanat eivät ole samat"
+        return "Passwords do not match"
     password_hash = generate_password_hash(password1)
 
     try:
         sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
         db.execute(sql, [username, password_hash])
     except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
+        return "Username is already taken"
 
-    return "Tunnus luotu"
+    return "Account created"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        sql = "SELECT password_hash FROM users WHERE username = ?"
+        password_hash = db.query(sql, [username])[0][0]
+
+        if check_password_hash(password_hash, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return "Incorrect username or password"
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")

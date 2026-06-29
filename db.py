b89e4bd81c0ce0195983746c194ab0b901ad1bf8
izsbe <isabella.petsalo@me.com>
2026-06-29 15:33:40 +0300

    Bug fix: always call connection close

--- db.py
+++ db.py
@@ -9,16 +9,22 @@ def get_connection():
 
 def execute(sql, params=[]):
     con = get_connection()
-    result = con.execute(sql, params)
-    con.commit()
-    g.last_insert_id = result.lastrowid
-    con.close()
+    try:
+        result = con.execute(sql, params)
+        con.commit()
+        g.last_insert_id = result.lastrowid
+    except Exception:
+        con.rollback()
+        raise
+    finally:
+        con.close()
 
 def last_insert_id():
     return g.last_insert_id    
     
 def query(sql, params=[]):
     con = get_connection()
-    result = con.execute(sql, params).fetchall()
-    con.close()
-    return result
+    try:
+        return con.execute(sql, params).fetchall()
+    finally:
+        con.close()
